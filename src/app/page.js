22905c2a87941ec868ef6e16aa76a6405f32f58a
sreyas <sreyas1045@gmail.com>
2024-10-25 23:58:03 +0530

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <main className="flex flex-row flex-wrap text-center   w-screen bg-bg min-h-screen max-h-screen overflow-y-hidden">
       <div className=" w-screen  flex justify-center  flex-col items-center text-gray-300 text-2xl gap-5">
-        <p className="text-[80px] font-bold text-[#ffffff]  bg-gradient-to-[#FFC107]">
+        <p className="text-[80px] font-bold text-[#ffffff] p-3  bg-gradient-to-[#FFC107]">
           Grammr
         </p>{" "}
-        Your AI-powered tool for perfecting your writing.
+       <p className="text-gray-400">
+       Your AI-powered tool for perfecting your writing.
+       </p>
         
         
         <Link
